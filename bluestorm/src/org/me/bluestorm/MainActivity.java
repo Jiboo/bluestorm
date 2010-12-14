40af609b44625ffc39d0e56222048b15cf2566ce
Lepesme 'Jiboo' Jean-Baptiste <lepesme.jb@gmail.com>
2010-12-14 10:17:22 +0100

    Modif sur le manifest et le main activity, l'appli est fonctionnelle.

--- bluestorm/src/org/me/bluestorm/MainActivity.java
+++ bluestorm/src/org/me/bluestorm/MainActivity.java
@@ -6,17 +6,20 @@
 package org.me.bluestorm;
 
 import android.app.Activity;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import java.io.IOException;
+import org.me.bluestorm.Capteurs.Capteurs;
+import org.me.bluestorm.Capteurs.tools.interfaces.IOrientationListener;
 
 /**
  *
  * @author jiboo
  */
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements IOrientationListener
 {
-    // TODO Initialiser cette variable, et appeler connect sur un evenement IHM
     Nxt nxt;
 
     public void applySpeed(float[] orientation) throws IOException
@@ -39,7 +42,7 @@ public class MainActivity extends Activity
         if(rightPower > 100) rightPower = 100;
         if(rightPower < -100) rightPower = -100;
 
-        Log.d("bluestorm", String.format("%.2f %.2f %.2f\n%3.0f %3.0f\n%3.0f %3.0f", orientation[0], orientation[1], orientation[2], accel, orient, leftPower, rightPower));
+        Log.d("bluestorm", String.format("applySpeed: %3.0f %3.0f", leftPower, rightPower));
         this.nxt.setSpeed((byte)leftPower, (byte)rightPower);
     }
 
@@ -47,7 +50,64 @@ public class MainActivity extends Activity
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here        
+
+        try
+        {
+            Capteurs sensorManager = new Capteurs((SensorManager) getSystemService(SENSOR_SERVICE));
+
+            this.nxt = new Nxt();
+
+            //TODO Utiliser un bouton pour permettre la connexion et demarrer l'ecoute sur le capteur d'orientation
+            this.nxt.connect();
+
+            sensorManager.subscribeOrientation(this, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        catch(Exception e)
+        {
+            //TODO Afficher un message a l'utilisateur
+            Log.e("bluestorm", "Exception caught in onCreate:", e);
+        }
     }
 
+    /*@Override
+    public void onResume() {
+        sensorManager.subscribeOrientation(this, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        sensorManager.subscribeOrientation(this, SensorManager.SENSOR_DELAY_NORMAL);
+    }*/
+
+    @Override
+    public void onDestroy() {
+        try
+        {
+            if(this.nxt != null)
+            {
+                if(this.nxt.isConnected())
+                {
+                    this.nxt.stop();
+                    this.nxt.close();
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            //TODO Afficher un message a l'utilisateur
+            Log.e("bluestorm", "Exception caught in onDestroy:", e);
+        }
+    }
+
+    public void getOrientation(SensorEvent event) {
+        try
+        {
+            this.applySpeed(event.values);
+        }
+        catch(Exception e)
+        {
+            //TODO Afficher un message a l'utilisateur
+            Log.e("bluestorm", "Exception caught in getOrientation:", e);
+        }
+    }
 }
