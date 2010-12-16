3cc4ddb1fd59f4d8d16d479d04ba0a050cf7d6b9
Lepesme 'Jiboo' Jean-Baptiste <lepesme.jb@gmail.com>
2010-12-16 09:44:05 +0100

    On avance sur l'ui. Suppression de la mise en forme de Nxt.java, sa faisait trop genre c'est pas un étudiant qui l' a écrit :p

--- bluestorm/src/org/me/bluestorm/Bluestorm.java
+++ bluestorm/src/org/me/bluestorm/Bluestorm.java
@@ -6,96 +6,71 @@
 package org.me.bluestorm;
 
 import android.app.Activity;
+import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import org.me.bluestorm.Capteurs.Capteurs;
+import org.me.bluestorm.Capteurs.tools.interfaces.IVirtualSensorWheelPowerListener;
 import org.me.bluestorm.ui.Home;
 
 /**
  *
  * @author jiboo
  */
-public class Bluestorm extends Activity
+public class Bluestorm extends Activity implements IVirtualSensorWheelPowerListener
 {
     Nxt nxt;
-<<<<<<< HEAD
     Capteurs capteurs;
-    
-=======
 
-    public void applySpeed(float[] orientation) throws IOException
-    {
-        double leftPower, rightPower;
-
-        double accel = (orientation[2]/50.) * 100;
-        double orient = (orientation[1]/50.) * 50;
-
-        leftPower = accel;
-        rightPower = accel;
-
-        if(orientation[1] > 0) {
-            leftPower -= orient;
-        } else {
-            rightPower += orient;
-        }
-
-        if(leftPower > 100) leftPower = 100;
-        if(leftPower < -100) leftPower = -100;
-        if(rightPower > 100) rightPower = 100;
-        if(rightPower < -100) rightPower = -100;
-
-        Log.d("bluestorm", String.format("applySpeed: %3.0f %3.0f", leftPower, rightPower));
-        this.nxt.setSpeed((byte)leftPower, (byte)rightPower);
-    }
-
->>>>>>> 5c2f08cc25170fcb5810c85577e29843565bbcb7
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        try
-        {
-            //capteurs = new Capteurs((SensorManager) getSystemService(SENSOR_SERVICE));
-            //this.nxt = new Nxt();
+        try {
+            capteurs = new Capteurs((SensorManager) getSystemService(SENSOR_SERVICE));
+            this.nxt = new Nxt();
 
             this.setContentView(new Home(this));
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             //TODO Afficher un message a l'utilisateur
             Log.e("bluestorm", "Exception caught in onCreate:", e);
         }
     }
 
-    /*@Override
-    public void onResume() {
-        capteurs.subscribeOrientation(this, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onPause() {
-        capteurs.subscribeOrientation(this, SensorManager.SENSOR_DELAY_NORMAL);
-    }*/
-
     @Override
     public void onDestroy() {
-        try
-        {
-            if(this.nxt != null)
-            {
-                if(this.nxt.isConnected())
-                {
+        super.onDestroy();
+
+        try {
+            if(this.nxt != null) {
+                if(this.nxt.isConnected()) {
                     this.nxt.stop();
                     this.nxt.close();
                 }
             }
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             //TODO Afficher un message a l'utilisateur
             Log.e("bluestorm", "Exception caught in onDestroy:", e);
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        capteurs.subscribe(Capteurs.TypesCapteurs.vWhellPower, this, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        capteurs.unsubscribe(Capteurs.TypesCapteurs.vWhellPower, this);
+    }
+
+    public void getWheelPower(SensorEvent event) {
     }
 }
--- bluestorm/src/org/me/bluestorm/ui/Home.java
+++ bluestorm/src/org/me/bluestorm/ui/Home.java
@@ -1,7 +1,9 @@
 package org.me.bluestorm.ui;
 
 import android.content.Context;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -10,24 +12,29 @@ import android.widget.TextView;
  *
  * @author jiboo
  */
-public class Home extends LinearLayout
-{
+public class Home extends LinearLayout implements OnClickListener {
     TextView text;
     Button connexion;
 
-    public Home(Context con)
-    {
+    public Home(Context con) {
         super(con);
         this.setOrientation(LinearLayout.HORIZONTAL);
         this.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
 
         this.connexion = new Button(con);
         this.connexion.setText("Connexion");
+        this.connexion.setOnClickListener(this);
 
         this.text = new TextView(con);
         this.text.setText("Vous devez avoir le NXT associé, et son nom doit être \"NXT\"");
 
         this.addView(this.text);
         this.addView(this.connexion);
+    }
+    
+    public void onClick(View arg) {
+        if(arg == connexion) {
+            Log.d("bluestorm", "Connexion button clicked!");
+        }
     }
 }
